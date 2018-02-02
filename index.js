566135cf28aedcba5d092fa1f1bf7e6808388890
Cherie Lum <charismatic.cherie@gmail.com>
2018-02-02 17:41:27 -0500

    adding exercise

--- index.js
+++ index.js
@@ -138,3 +138,26 @@ var laugh = function (numberOfLaughs){
 
 console.log(laugh(10));
 
+// Write a named function expression that stores the function in a variable called cry and returns "boohoo!".
+
+var cry = function crying() {
+  return "boohoo!"; 
+}
+
+console.log(cry());
+
+// Call the emotions() function so that it prints the output you see below, but instead of passing the laugh() function as an argument, pass an inline function expression instead.
+
+function emotions(myString, myFunc) {
+  console.log("I am " + myString + ", " + myFunc(2));
+}
+
+emotions("happy", function laugh(num){
+  var laugh = ""; 
+  for (i = 0; i < num; i++){
+      laugh += "ha";
+  } 
+  return laugh +"!";
+}
+);
+
